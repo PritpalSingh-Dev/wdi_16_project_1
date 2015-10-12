1b956042ad4e30dd967146b85ca344295ae3fd06
psbadewal <psbadewal@icloud.com>
2015-10-12 16:13:31 +0100

    Managed to build timer and re-wrote code

--- js/app.js
+++ js/app.js
@@ -18,93 +18,97 @@ window.onload = function() {
   var player1Score = 0;
   var round  = 0;
 
-  $("#target").on("submit", validateForm);
-
-  $("button").on("click", function(){
-    getRandomLetter(startTimer);
+  $("#target").on("submit", function(){
+    stopTimer();
+    validateForm();
+    $("#displayScoreId").html("Score: " + player1Score);
+    clearBoard();
+    getRandomLetter();
   });
 
-  function getRandomLetter() {
+  $("button").on("click", startGame);
+
+
+
+
+  function startGame() {
+    $("#letterGenerator").css("display", "none")
+    startTimer();
+    getRandomLetter();
+  }
+
+  function getRandomLetter(){
     var alphabet = ['A','B','C'];
     var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
     $("#test").html(randomLetter);
     $("#country").val(randomLetter);
     $("#capital_City").val(randomLetter);
     $("#currency").val(randomLetter);
-
-    // Start timer
-    startTimer();
   }
 
-  //function startTimer(){
-  //  console.log("GO")
-//
-  //  // When timer runs out -> clearBoard
-  //}
 
-   
-        i = 45;
-        function startTimer() {
-          document.getElementById('countdown').innerHTML = i;
-          i--;
-          if (i <= 0) {
-            alert(player1Score);
-          }
-          else {
-            setTimeout(startTimer, 1000);
-          }
-        }
-     
-      startTimer();
-
-
+  var i=25;
+  function startTimer() {
+    document.getElementById('countdown').innerHTML = i;
+    i--;
+    if (i < 0) {
+      clearBoard();
+      console.log(player1Score + " You ran out of time.");
+      stopTimer();
+      $("#test").html("You ran out of time, try again")
+      $("#letterGenerator").css("display", "")
+    }
+    else {
+      setTimeout(startTimer, 1000);
+    }
+  }
 
   function stopTimer(){
-    if ($("#target").on("submit")) {
-    clearInterval(i)
+    clearTimeout();
+    i=25;
   };
-//The timer should stop when user submits form.
-//The timer should also stop when user doesn't submit form 
-  }
 
   function clearBoard(){
-    $("target")[0].reset(); 
+    $("#test").html("");
+    $("#country").val("");
+    $("#capital_City").val("");
+    $("#currency").val("");
   }
 
-  function validateForm() {
-    event.preventDefault();
+//The timer should stop when user submits form.
+//The timer should also stop when user doesn't submit form 
 
-    var country1 = $("#country").val();
-    var capititalCity1 = $("#capital_City").val();
-    var currency1 = $("#currency").val();
-    player1Score;
 
-    if (countries.indexOf(country1)>-1) {
-      player1Score++;
-    } else {
-      player1Score;
-    }
+function validateForm() {
+  event.preventDefault();
 
-    if (capitalCities.indexOf(capititalCity1)>-1) {
-      player1Score++;
-    } else {
-      player1Score;
-    }
+  var country1 = $("#country").val();
+  var capititalCity1 = $("#capital_City").val();
+  var currency1 = $("#currency").val();
 
-    if (currencies.indexOf(currency1)>-1) {
-      player1Score++;
-    } else {
-      player1Score;
-    }
-
-    $("#displayScoreId").html("Score: " + player1Score);
+  if (countries.indexOf(country1)>-1) {
+    player1Score++;
   }
 
-  var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Arzebaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Carpe Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Cook Islands", "Croatia", "Cuba", "Cyprus", "Czech Republic"];
+  if (capitalCities.indexOf(capititalCity1)>-1) {
+    player1Score++;
+  }
 
-  var capitalCities = ["Abu Dhabi", "Abuja", "Accra", "Adamstown", "Addis Ababa", "Algiers", "Alofi", "Ankara", "Amman", "Amsterdam", "Andorra", "Antananarivo", "Apia", "Ashgabat", "Asmara", "Astana", "Asuncion", "Athens", "Avaru", "Baghdad", "Baku", "Bamako", "Bandar Seri Begawan", "Bangkok", "Bangui", "Banjul", "Basse-Terre", "Basse-Terre", "Beijing", "Beirut", "Belgrade", "Belmopan", "Berlin", "Bern", "Bishkek", "Bissau", "Bloemfontein","Bogotá", "Brasília", "Bratislava", "Brazzaville", "Bridgetown", "Brussels", "Bucharest", "Budapest", "Buenos Aires", "Bujumbura", "Cairo", "Canberra", "Cape Town", "Caracas", "Castries", "Cayenne", "Charlotte Amalie", "Chisinau", "Cockburn Town", "Canakry", "Copenhagen",];
+  if (currencies.indexOf(currency1)>-1) {
+    player1Score++;
+  }
 
-  var currencies = ["Argentine Peso", "Arubian Guilder", "Australian Dollar", "Armenian Dram", "Azerbaijan Manat", "Bahamian Dollar", "Bahraini Dinar", "Bangladeshi Taka", "Barbadian Dollar", "Barbados Dollar", "Belize Dollar", "Bermudian Dollar", "Bhutan Ngultrum", "Bolivia Boliviano", "Botswana  Pula", "Brazilian Real", "Brunei Dollar", "Bulgarian Lev", "Burmese Kyat", "Cape Verde Escudo", "Cambodia  Riel", "Canadian Dollar", "Chilean Peso", "Chinese Yuan"]; 
+  console.log(player1Score)
+
+  console.log("submitted")
+}
+
+var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Arzebaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Carpe Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Cook Islands", "Croatia", "Cuba", "Cyprus", "Czech Republic"];
+
+var capitalCities = ["Abu Dhabi", "Abuja", "Accra", "Adamstown", "Addis Ababa", "Algiers", "Alofi", "Ankara", "Amman", "Amsterdam", "Andorra", "Antananarivo", "Apia", "Ashgabat", "Asmara", "Astana", "Asuncion", "Athens", "Avaru", "Baghdad", "Baku", "Bamako", "Bandar Seri Begawan", "Bangkok", "Bangui", "Banjul", "Basse-Terre", "Basse-Terre", "Beijing", "Beirut", "Belgrade", "Belmopan", "Berlin", "Bern", "Bishkek", "Bissau", "Bloemfontein","Bogotá", "Brasília", "Bratislava", "Brazzaville", "Bridgetown", "Brussels", "Bucharest", "Budapest", "Buenos Aires", "Bujumbura", "Cairo", "Canberra", "Cape Town", "Caracas", "Castries", "Cayenne", "Charlotte Amalie", "Chisinau", "Cockburn Town", "Canakry", "Copenhagen",];
+
+var currencies = ["Argentine Peso", "Arubian Guilder", "Australian Dollar", "Armenian Dram", "Azerbaijan Manat", "Bahamian Dollar", "Bahraini Dinar", "Bangladeshi Taka", "Barbadian Dollar", "Barbados Dollar", "Belize Dollar", "Bermudian Dollar", "Bhutan Ngultrum", "Bolivia Boliviano", "Botswana  Pula", "Brazilian Real", "Brunei Dollar", "Bulgarian Lev", "Burmese Kyat", "Cape Verde Escudo", "Cambodia  Riel", "Canadian Dollar", "Chilean Peso", "Chinese Yuan"]; 
+
 };
 
 //5. If player enters value and submits value, check starting value against the random character, then loop through each of the arrays to find whether the capital city, country and rivers are correct. Add an eventlistener
